98563f1b67886634fb84f9bff46035e37504d190
ThinkNaive <30410985+ThinkNaive@users.noreply.github.com>
2022-04-27 11:16:46 +0800

    Initial commit

---
+++ Template/Plugin.cpp
@@ -0,0 +1,18 @@
+#include "pch.h"
+#include <EventAPI.h>
+#include <LoggerAPI.h>
+#include <MC/Level.hpp>
+#include <MC/BlockInstance.hpp>
+#include <MC/Block.hpp>
+#include <MC/BlockSource.hpp>
+#include <MC/Actor.hpp>
+#include <MC/Player.hpp>
+#include <MC/ItemStack.hpp>
+#include <LLAPI.h>
+
+static_assert(false, "Please set your plugin's name below and delete this line");
+Logger logger("PluginName");
+
+void PluginInit()
+{
+}
